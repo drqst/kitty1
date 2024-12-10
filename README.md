2211a1e7a7d5d73e3e3b83a37fc2b7b8cbe97c22
Daniel Rönnqvist <daniel.ronnqvist@peratech.com>
2024-12-10 14:12:05 +0100

    Add diagonal check and short info in the README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kitty1
+
+Problem A:
+
+Run like this from src/dir: 
+
+    python main.py ..\test\sample-01.in
+
+Can't get the .in and .ans to match, in sample-01.in the last query is "1 1 10 20" which should result in decimal since both positions are 1's, and it's not a diagonal but the .ans tells me it should be "neither".
--- src/main.py
+++ src/main.py
@@ -10,7 +10,17 @@ def load_input(filename):
         header = line.split(" ")
         rows = int(header[0])
         for i in range(rows):
-            input_map.append(f.readline().strip())
+            if i > 1000:
+                print("To many input lines")
+                exit(1)
+
+            line = f.readline().strip()
+            if len(line) > 1000:
+                print("To big input line")
+                exit(1)
+
+            input_map.append(line)
+
 
         number_of_querys = int(f.readline().strip())
 
@@ -25,10 +35,28 @@ def run_querys(input_matrix, querys):
     result = ""
     for query in querys:
         query_list = query.split(" ")
-        from_row = int(query_list[0]) - 1
-        from_col = int(query_list[1]) - 1
-        to_row = int(query_list[2]) - 1
-        to_col = int(query_list[3]) - 1
+        from_row = int(query_list[0]) - 1 # zero indexed.
+        from_col = int(query_list[1]) - 1 # zero indexed.
+        to_row = int(query_list[2]) - 1 # zero indexed.
+        to_col = int(query_list[3]) - 1 # zero indexed.
+
+        # Check that query doesn't move diagonal
+
+        # print(f"From Row: {input_matrix[from_row]}")
+        # print(f"To Row:   {input_matrix[to_row]}")
+        # print(f"From {from_row + 1, from_col + 1} / To {to_row + 1, to_col + 1}")
+
+        try:
+            slope = ((to_col + 1) - (from_col + 1)) / ((to_row + 1) - (from_row + 1))
+            # print(f"Slope: {slope}")
+            if abs(slope) == 1:
+                result += "neither"
+                continue
+        except ZeroDivisionError:
+            # Not a diagonal
+            pass
+
+            
 
         try:
             from_value = int(input_matrix[from_row][from_col])
@@ -37,9 +65,7 @@ def run_querys(input_matrix, querys):
             result += "neither, out of bounds\n"
             continue
 
-        print(f"From Row: {input_matrix[from_row]}")
-        print(f"To Row:   {input_matrix[to_row]}")
-        print(f"From {from_row, from_col} / To {to_row, to_col} / Vals {from_value, to_value}")
+        # print(f"From {from_row, from_col} / To {to_row, to_col} / Vals {from_value, to_value}")
 
         if s := from_value == to_value and from_value == 1:
             result += "decimal\n"
@@ -57,8 +83,8 @@ def main():
         filename =  args[0]
         input_matrix = load_input(filename)
         q = run_querys(input_matrix[1], input_matrix[2])
-        print(f"Input: {input_matrix[1]}")
-        print(f"Output: {q}")
+        # print(f"Input: {input_matrix[1]}")
+        print(f"{q}")
     else:
         print ("No arguments")
 
