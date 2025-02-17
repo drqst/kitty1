845722dd6a8ea22c948800eb8ed0581df34b7a52
Daniel Rönnqvist <daniel.ronnqvist@peratech.com>
2025-02-17 04:09:50 +0100

    Read from stdin

--- src/main.py
+++ src/main.py
@@ -1,44 +1,13 @@
 import sys
 
-def load_input():
-    input_map = []
-    querys = []
-
-    headers_for_data = sys.stdin.readline()
-
-    numbers_of_data_rows = hedaers_for_data[0]
-    numbers_of_data_columns = hedaers_for_data[1]
-
-    # if j > 1000:
-    #     print("To many input colums")
-    #     exit(1)
-
-    # if len(data_line[]) > 1000:
-    #     print("To big input line")
-    #     exit(1)
-
-    for (i in range(0, number_of_data_lines)):
-        input_map.append(sys.stdin.readline(data_line))
-
-    headers_for_querys = sys.stdin.readline()
-    number_of_querys = headers_for_querys[0]
-
-    for i in range(number_of_querys):
-        querys.append(f.readline().strip())
-
-    complete_array = [input_map, querys]
-
-    return complete_array
-
 def run_querys(input_matrix, querys):
+    debug = ""
     result = ""
     column_count = len(input_matrix[0])
     row_count = len(input_matrix)
+    diagonal_slope = len(input_matrix[0]) / len(input_matrix)
 
-    result += f"Input Matrix: {input_matrix}\n"
-    # result += f"Column count of matrix {column_count}\n"
-    # result += f"Row count of matrix {row_count}\n"
-
+    debug += f"Matrix diagonal slope {diagonal_slope}\n"
 
     for query in querys:
         query_list = query.split(" ")
@@ -48,36 +17,28 @@ def run_querys(input_matrix, querys):
         to_col = int(query_list[3]) - 1
 
     
-        result += f"From Row: {input_matrix[from_row]}, Query: {query_list[0]} {query_list[1]} {query_list[2]} {query_list[3]}\n"
-        result += f"To Row:   {input_matrix[to_row]}\n"
+        debug += f"From Row: {input_matrix[from_row]}, To Row: {input_matrix[to_row]}, Query: {query_list[0]} {query_list[1]} {query_list[2]} {query_list[3]}\n"
     
-        result += f"column count {column_count}, row count {row_count}\n"
-
         diagonal_slope = column_count / row_count
 
-        result += f"diagonal slope {diagonal_slope}\n"
-    
+   
         # Check that query doesn't move diagonal
         try:
             slope = abs( ((to_col ) - (from_col)) / ((to_row) - (from_row)) )
-            result += f"Slope: {slope}, to col {to_col}, from col {from_col}, to row {to_row} from row {from_row}\n"
+            debug += f"Slope: {slope}, from {from_col}, {from_row} to {to_col}, {to_row}, \n"
             if abs(slope) == diagonal_slope:
                 result += "neither\n"
                 continue
         except ZeroDivisionError:
-            # Not a diagonal
+            debug += "Not a diagonal\n"
             pass
-
-            
 
         try:
             from_value = int(input_matrix[from_row][from_col])
             to_value = int(input_matrix[to_row][to_col])
         except IndexError:
-            result += "neither, out of bounds\n"
+            debug += "neither, out of bounds\n"
             continue
-
-        result += f"Divided by {from_row, from_col} / To {to_row, to_col} / Vals {from_value, to_value}\n"
 
         if s := from_value == to_value and from_value == 1:
             result += "decimal\n"
@@ -86,38 +47,54 @@ def run_querys(input_matrix, querys):
         else:
             result += "neither\n"
 
+    print(debug)
+
     return result
-f
+
 
 def load_input():
     result = ""
-    input_map = []
+    input_matrix = []
     querys = []
 
     import sys
+    import readline
 
-    for line in sys.stdin:
-        header = line.split(" ")
-        rows = int(header[0])
-        for i in range(rows):
-            if i > 1000:
-                result = "To many input lines"
-                exit(1)
+    header_line = sys.stdin.readline().strip()
+    header = header_line.split()
+    input_rows = int(header[0])
 
-            line = f.readline().strip()
-            if len(line) > 1000:
-                result = "To big input line"
-                exit(1)
+    # print(input_rows)
 
-            input_map.append(line)
+    if input_rows > 1000:
+        print(f"To many input lines {i}")
+        exit(1)
 
-    return result
+    for i in range(input_rows):
+
+        line = sys.stdin.readline().strip()
+
+        if len(line) > 1000:
+            print(f"To big input line {len(line)}")
+            exit(1)
+
+        input_matrix.extend(line.split())
+
+    querys_count = int(sys.stdin.readline().strip())
+
+    for i in range(querys_count):
+        querys.append(sys.stdin.readline().strip())
+
+    # print(input_matrix)
+    # print(querys)
+
+    return input_matrix, querys
 
 
 def main():
-    complete_array = load_input()
-    result = run_querys(complete_array)
-    print(result)ertdghsdf
+    input_data, querys = load_input()
+    result = run_querys(input_data, querys)
+    print(result)
 
 if __name__ == "__main__":
     main()
